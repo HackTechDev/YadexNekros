81872b29b80d335420141e0b045936fad15a000d
LeSanglier <lesanglierdesardennes@gmail.com>
2012-11-04 12:10:31 +0100

    Initial release : v0.0.1 and Yadex 1.7.1

---
+++ compilerYadex.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+make CC=gcc-3.4
---
+++ src/version.cc
@@ -0,0 +1,4 @@
+// DO NOT EDIT -- generated from VERSION
+
+extern const char *const yadex_source_date = "2012-08-20";
+extern const char *const yadex_version = "1.7.0";
